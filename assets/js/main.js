4173b8473bdd82eb9c5f3300556f84c091e832e3
bingyu-zhou <sunnyzhou97@gmail.com>
2019-11-01 13:34:05 +1100

    index and backup

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+$( document ).ready(function() {
+    console.log( "im ready!" );
+
+    $('.btn-home').click(function() {
+        $('.right').removeClass('active');
+        $('.panel-homepage').addClass('active');
+    });    
+
+    $('.btn-hwc').click(function() {
+    	$('.right').removeClass('active');
+    	$('.panel-hwc').addClass('active');
+    });
+
+    $('.btn-mmxix').click(function() {
+    	$('.right').removeClass('active');
+    	$('.panel-mmxix').addClass('active');
+    });
+
+    $('.btn-mada').click(function() {
+    	$('.right').removeClass('active');
+    	$('.panel-mada').addClass('active');
+    });
+
+    $('.btn-msdx').click(function() {
+    	$('.right').removeClass('active');
+    	$('.panel-msdx').addClass('active');
+    });
+
+    $('.btn-gradx').click(function() {
+    	$('.right').removeClass('active');
+    	$('.panel-gradx').addClass('active');
+    });                
+
+    $('.btn-gradshow').click(function() {
+    	$('.right').removeClass('active');
+    	$('.panel-gradshow').addClass('active');
+    }); 
+
+});
